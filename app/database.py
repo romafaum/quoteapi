fc279fedea66ee0fead252522320a288ee9df877
Roman <romafaum1@gmail.com>
2024-03-19 16:14:22 +0200

    Created db with quotes

--- app/database.py
+++ app/database.py
@@ -1,7 +1,9 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from app.setts import settings
+from setts import settings
 
 SQLALCHEMY_DATABASE_URL = f"postgresql+psycopg://{settings.user}:{settings.password}@{settings.host}/{settings.dbname}"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
--- app/models.py
+++ app/models.py
@@ -8,8 +8,8 @@ class Base(DeclarativeBase):
 tags_to_quotes_table = Table(
     "tags_and_quotes",
     Base.metadata,
-    Column('quote_id', ForeignKey("quotes.id"), primary_key=True),
-    Column('tags_id', ForeignKey("tags.id"), primary_key=True)
+    Column('quote_id', ForeignKey("quotes.id", ondelete="CASCADE"), primary_key=True),
+    Column('tags_id', ForeignKey("tags.id", ondelete="CASCADE"), primary_key=True)
 )
 
 class Quotes(Base):
@@ -18,7 +18,7 @@ class Quotes(Base):
     id: Mapped[int] = mapped_column(primary_key=True)
     quote: Mapped[Text] = mapped_column(Text())
     author_id: Mapped[int] = mapped_column(ForeignKey('authors.id', ondelete="CASCADE"))
-    source_id: Mapped[int | None] = mapped_column(ForeignKey('authors.id'))
+    source_id: Mapped[int | None] = mapped_column(ForeignKey('sources.id'))
     author: Mapped["Author"] = relationship(back_populates="quotes")
     sourse: Mapped[Optional["Source"]] = relationship(back_populates="quotes")
     tags: Mapped[Optional[List["Tag"]]] = relationship(secondary=tags_to_quotes_table, back_populates="quotes")
@@ -28,16 +28,19 @@ class Author(Base):
     __tablename__ = "authors"
 
     id: Mapped[int] = mapped_column(primary_key=True)
-    fullname: Mapped[str] = mapped_column(String(80))
+    fullname: Mapped[str] = mapped_column(String(300), unique=True)
+    quotes: Mapped[Optional["Quotes"]] = relationship(back_populates="author")
 
 class Source(Base):
     __tablename__ = "sources"
 
     id: Mapped[int] = mapped_column(primary_key=True)
-    name: Mapped[str] = mapped_column(String(250))
+    name: Mapped[str] = mapped_column(String(800), unique=True)
+    quotes: Mapped[Optional["Quotes"]] = relationship(back_populates="sourse")
 
 class Tag(Base):
     __tablename__ = "tags"
 
     id: Mapped[int] = mapped_column(primary_key=True)
-    name: Mapped[str] = mapped_column(String(300))
+    name: Mapped[str] = mapped_column(String(300), unique=True)
+    quotes: Mapped[Optional[List["Quotes"]]] = relationship(secondary=tags_to_quotes_table, back_populates="tags")
